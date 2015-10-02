16082b81190502a639ac3cfb87366058557bf726
Rafael Zegarra <rafael.zegarra@ucsp.edu.pe>
2015-10-02 11:55:18 -0500

    config
    archivo con claves para Facebook y twitter

---
+++ login_nodejs/config.js
@@ -0,0 +1,12 @@
+var config={
+	twitter :{
+		key:'c5adDwL9qVkTLWlDeeAdLmlnq',
+		secret:'ztzxtDs0v3MKc8tgyuMLJUQKWausmx9grsoDKEK4s8vcSB977T'
+	},
+
+	facebook :{
+		id:'454929544709548',
+		secret:'962fe040869edb3975ef320b89d7e0f0'
+	}
+};
+module.exports = config;
