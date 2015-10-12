b09e650dd0ccf27cc7ec3b0b62b210cbd27affef
Rafael Zegarra <rafael.zegarra@ucsp.edu.pe>
2015-10-11 23:37:30 -0500

    pruebas
    diseño de login , css, html5

--- login_nodejs/routes/index.js
+++ login_nodejs/routes/index.js
@@ -7,7 +7,7 @@ exports.index = function(req, res){
     // Enviamos como variables un título
     // y objeto 'user' que contiene toda
     // la información del usuario y viaja en el 'request'
-    title: 'Ejemplo de Passport JS',
+    title: 'Amica :) assistant',
     user: req.user
   });
 };
